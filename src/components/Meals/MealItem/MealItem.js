5dd5dca7d65a4c0fce160cf96bcbe68f4567fe18
Arindam01gi <1304arindam@gmail.com>
2023-01-17 19:59:25 +0530

    Up to Managing Cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -12,7 +12,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-      <MealItemFrom />
+      <MealItemFrom id={props.id}/>
       </div>
     </li>
   );
